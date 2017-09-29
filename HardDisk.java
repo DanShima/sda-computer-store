ae20233f5bebaf2127b7b63e0662c74d72144050
molock10 <molock10@hotmail.com>
2017-09-29 16:22:28 +0200

    Update HardDisk.java

--- HardDisk.java
+++ HardDisk.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class HardDisk extends Components
+public class HardDisk extends Component
 {
     // instance variables - replace the example below with your own
     private String StorageCapacity;
